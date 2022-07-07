aa2ab3dce08a36adf37ac0b371b39c917ac0a3d8
reenhilm <christian@kajal.se>
2022-07-07 10:00:31 +0200

    Färdig med feauture

--- GitTestar/Extensions.cs
+++ GitTestar/Extensions.cs
@@ -3,5 +3,7 @@
     public static class Extensions
     {
         //This is where we keep extensions
+        //jag önskar jag hade line 33 i Program nu
+        //bra nu använder jag line 33 och blev färdig
     }
 }
